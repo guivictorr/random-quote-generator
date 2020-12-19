c7240da0ee5ad60e74fcdc0b85cd2278f717a493
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2020-12-19 16:04:35 -0300

    api update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://quote-garden.herokuapp.com/api/v2/'
+  baseURL: 'https://quote-garden.herokuapp.com/api/v3/'
 })
 
 export default api
